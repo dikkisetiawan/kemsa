eabd15241460f3f47c5dbf30e970e91e343b96a7
initialds <initialsds@gmail.com>
2022-09-24 20:57:06 +0700

    ganti rentang harga ke class

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:kemsa/widgets/date_time_picker_timeline.dart';
+import '../widgets/date_time_picker_timeline.dart';
+import '../widgets/slider_rentang_harga.dart';
 import '../themes/light_color.dart';
 import '../themes/theme.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
@@ -22,7 +22,7 @@ class _FiltersState extends State<Filters> {
   late final tileContent = [
     checkboxJenisLayanan(),
     selectRating(),
-    sliderRentangHarga(),
+    SliderRentangHarga(),
     DateTimePickerTimeline()
   ];
 
@@ -78,79 +78,6 @@ class _FiltersState extends State<Filters> {
           half: Icon(Icons.star_half_outlined, color: LightColor.yellowColor),
           empty: Icon(Icons.star_border, color: LightColor.yellowColor)),
       onRatingUpdate: (rating) {},
-    );
-  }
-
-  Widget sliderRentangHarga() {
-    RangeValues values = RangeValues(30, 70);
-    RangeLabels labels = RangeLabels('1', "100");
-
-    return Column(
-      children: [
-        Row(
-          children: [
-            Flexible(
-              flex: 2,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: LightColor.darkgrey,
-                          width: 3,
-                        ),
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: "Terendah",
-                    hintStyle: AppTheme.h4Style,
-                    prefixIcon: Icon(
-                      Icons.money_off_csred_outlined,
-                      size: 20,
-                    )),
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ], // Only numbers can be entered
-              ),
-            ),
-            Spacer(flex: 1),
-            Flexible(
-              flex: 2,
-              child: TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: LightColor.darkgrey,
-                          width: 3,
-                        ),
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: "Tertinggi",
-                    hintStyle: AppTheme.h4Style,
-                    prefixIcon: Icon(
-                      Icons.money_off_csred_outlined,
-                      size: 20,
-                    )),
-                keyboardType: TextInputType.number,
-                inputFormatters: [
-                  FilteringTextInputFormatter.digitsOnly,
-                ], // Only numbers can be entered
-              ),
-            )
-          ],
-        ),
-        RangeSlider(
-            activeColor: LightColor.semanticpink,
-            inactiveColor: LightColor.mainPink,
-            min: 1,
-            max: 100,
-            values: values,
-            labels: labels,
-            onChanged: (value) {
-              setState(() {
-                values = value;
-                labels = RangeLabels("${value.start.toInt().toString()}\$",
-                    "${value.start.toInt().toString()}\$");
-              });
-            }),
-      ],
     );
   }
 
